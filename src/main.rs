84efa4087d3742d38b05d0932ee43c9f6d264fdc
Bahar Demircan <kodyenler@hotmail.com>
2022-11-15 09:38:10 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+mod kitty;
+mod living;
+
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgba(0.0, 0.0,0.0,1.0)))
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+}
